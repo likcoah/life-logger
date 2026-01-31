c54f37aab726a4b60a325cac5f20f5176e0e1d02
likcoah <likcoah@gmail.com>
2026-01-31 20:53:03 +0500

    feat(common): add /start handler with automatic registration in the database

--- Backend/src/bot/handlers/common.py
+++ Backend/src/bot/handlers/common.py
@@ -1,3 +1,5 @@
+from src.database.db import db
+
 from aiogram import Router, types
 from aiogram.filters import CommandStart
 
@@ -6,8 +8,12 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def cmdStart(message: types.Message):
+async def cmd_start(message: types.Message):
     if not message.from_user:
         return
-    await message.answer(f'Hello, <b>{message.from_user.first_name}</b>')
+    if db.is_registered(message.from_user.id):
+        await message.answer(f'Welcome back, {message.from_user.first_name}')
+    else:
+        db.register_user(message.from_user.id)
+        await message.answer(f'Hi, {message.from_user.first_name}! Registration was successfull')
 
